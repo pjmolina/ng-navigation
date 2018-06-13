4ebd9cd2ef9e19538a13b7df2d0ee403ecab4495
Pedro J. Molina <pjmolina@gmail.com>
2018-06-13 14:25:27 +0200

    cambios a city component

--- src/app/city/city.component.ts
+++ src/app/city/city.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { City } from '../domain/city';
+import { ActivatedRoute, Params } from '@angular/router';
+import { CityService } from '../services/city.service';
 
 @Component({
   selector: 'app-city',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./city.component.css']
 })
 export class CityComponent implements OnInit {
+  city: City;
 
-  constructor() { }
+  constructor(private activedRoute: ActivatedRoute,
+              private cityService: CityService) { }
 
   ngOnInit() {
+      this.activedRoute.params.subscribe(
+        data => this.onParamChange(data)
+      );
+  }
+
+  onParamChange(data: Params) {
+    const id = data.id;
+
+    this.city = this.cityService.getCityById(id);
   }
 
 }
